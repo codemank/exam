eecd72abf645610fb697632024f8e890cc29260f
codemank <mayankraghu18@gmail.com>
2019-12-05 15:20:14 +0000

    second commit

--- calculator.cpp
+++ calculator.cpp
@@ -4,35 +4,31 @@ using namespace std;
 int main()
 {
 
-    int n1,n2,ch,r;
+    int n1=6,n2=2,ch=2,r;
 
     do{
-    cout<<"\n1.ADDITION\n 2.SUBSTRACTION\n 3.MULTIPLY\n 4.DIVISION\n 5.EXIT"<<endl;
-    cout<<"Enter your choice ";
+   // cout<<"\n1.ADDITION\n 2.SUBSTRACTION\n 3.MULTIPLY\n 4.DIVISION\n 5.EXIT"<<endl;
+//cout<<"Enter your choice ";
     cin>>ch;
 
         switch(ch)
         {
-                    case 1:  cout<<"\n Enter 2 nos ";
-                            cin>>n1>>n2;
+                    case 1:
                             r = n1 + n2;
                             cout<<"ADDITION "<<r;
                             break;
 
-                    case 2: cout<<"\n Enter 2 nos ";
-                            cin>>n1>>n2;
+                    case 2:
                             r = n1 - n2;
                             cout<<"SUBSTRACTION "<<r;
                             break;
 
-                    case 3: cout<<"\n Enter 2 nos ";
-                            cin>>n1>>n2;
+                    case 3:
                             r = n1 * n2;
                             cout<<"MULTIPLY "<<r;
                             break;
 
-                    case 4: cout<<"\n Enter 2 nos ";
-                            cin>>n1>>n2;
+                    case 4:
                             r = n1 / n2;
                             cout<<"DIVISION "<<r;
                             break;
